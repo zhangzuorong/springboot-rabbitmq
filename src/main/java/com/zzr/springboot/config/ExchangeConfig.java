e508fbae1d028ed1ce1b3843ef7d7aec58d05558
zhangzuorong <994255496@qq.com>
2019-05-10 10:04:38 +0800

    1.0

---
+++ src/main/java/com/zzr/springboot/config/ExchangeConfig.java
@@ -0,0 +1,36 @@
+package com.zzr.springboot.config;
+
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 开发公司：山东海豚数据技术有限公司
+ * 版权：山东海豚数据技术有限公司
+ * <p>
+ * ExchangeConfig
+ *
+ * @author zzr
+ * @created Create Time: 2019/5/10
+ */
+@Configuration
+public class ExchangeConfig {
+    //配置路由交换机 根据路由匹配转发消息给队列
+    @Bean(name = "exchangeOne")
+    public TopicExchange exchangeOne() {
+        return new TopicExchange("exchange.one");
+    }
+
+    //配置路由交换机
+    @Bean(name = "exchangeTwo")
+    public TopicExchange exchangeTwo() {
+        return new TopicExchange("exchange.two");
+    }
+
+    //配置交换机(广播) 转发消息给旗下所有队列
+    @Bean(name = "fanoutExchange")
+    FanoutExchange fanoutExchange() {
+        return new FanoutExchange("fanoutExchange");
+    }
+}
