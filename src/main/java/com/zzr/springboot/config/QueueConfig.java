e82f611fe6c75482e6e9523c36bf3d15970dc91d
zhangzuorong <994255496@qq.com>
2019-05-23 16:24:59 +0800

    3.0

--- src/main/java/com/zzr/springboot/config/QueueConfig.java
+++ src/main/java/com/zzr/springboot/config/QueueConfig.java
@@ -27,7 +27,9 @@ public class QueueConfig {
          auto-delete 表示消息队列没有在使用时将被自动删除 默认是false
          exclusive  表示该消息队列是否只在当前connection生效,默认是false
          */
-        return new Queue("springboot-queue-one",true,false,false);
+        Map<String,Object> args = new HashMap<>();
+        args.put("x-expires",18000);
+        return new Queue("springboot-queue-one",false,false,false,args);
     }
 
     @Bean
