39b570e9fe302b55ad0954fc5929ceee3f1a7db0
zhangzuorong <994255496@qq.com>
2019-06-15 09:40:03 +0800

    3.0

--- src/main/java/com/zzr/springboot/config/RabbitMqConfig.java
+++ src/main/java/com/zzr/springboot/config/RabbitMqConfig.java
@@ -1,12 +1,9 @@
 package com.zzr.springboot.config;
 
-
-import org.springframework.amqp.core.AcknowledgeMode;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
